60b74ef704c3971c5329c05566ff1a7fd36b9016
Tumble <tumblegamer@gmail.com>
2019-10-25 23:21:19 +0100

    Update json.js

--- server/json.js
+++ server/json.js
@@ -1,16 +1,34 @@
 const fs = require('fs');
 
+function JSONExists(file) {
+    try {
+      return fs.existsSync(path);
+    } catch(err) {
+        return false;
+    }
+}
+
 function ReadJSON(file) {
     return new Promise((resolve,reject)=>{
+    try {
+      if (fs.existsSync(path)) {
         fs.readFile(require.resolve(file), (err,data)=>{
-            if(err) {
-                reject(err);
-            } else {
-                resolve(JSON.parse(data));
-            }
-        });
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(JSON.parse(data));
+                }
+            });
+      }
+    } catch(err) {
+      reject(err)
+    }
+        
     });
     
 }
 
-module.exports = ReadJSON;
+module.exports = {
+ReadJSON,
+    JSONExists
+};
