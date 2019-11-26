2194b3063394a38d3eac77f42de2eb29b70fef67
Tumble <tumblegamer@gmail.com>
2019-11-26 18:21:44 +0000

    changed to tn-webserver

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const webserver = require('webserver');
+const WebServer = require('tn-webserver');
 
 global.appDir = path.dirname(require.main.filename);
 
 const app = require('./server/web');
 const boxCritters = require('./server/boxcritters');
 
-var server = webserver(app);
+var server = WebServer(app);
 boxCritters(server);
