401de50fadbd259278b7750da2f31821277ad722
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-04-22 21:47:50 -0400

    some comments

--- server/main.js
+++ server/main.js
@@ -3,11 +3,15 @@
 const socketIo = require('socket.io').listen(global.app, { 'pingInterval': 100, 'pingTimeout': 10000 })
 
 function Listener(session) {
-	this.session = session
+	/* creates socket.on for every listener function
+	*  some sockets aren't neccecary and are stuff like constructor() which may cause issues
+	*  this should be fixed in the future
+	*/
 	for (let i of Object.getOwnPropertyNames(Listener.prototype)) {
-		this.session.socket.on(i, this[i])
+		session.socket.on(i, this[i])
 		console.log(i)
 	}
+	this.session = session
 }
 
 const Player = require('./player')
@@ -26,15 +30,17 @@ function SetupSession(socket) {
 		room: undefined
 	}
 	socket.emit("connect")
-	var listener = new Listener(session)
+	var listener = new Listener(session) //listener creates
 }
 
 function main(server) {
 	{
+		/* grab all plugins and pass global.World through
+		*/
 		let plugins = require('../plugins/RunOrder')
 		for (let i of plugins) {
 			console.log(`loading plugin ${i}`)
-			if (/[\\\/:*?"<>|]/.exec(i)) throw `${i} isn't a valid directory name`
+			if (/[\\\/:*?"<>|]/.exec(i)) throw `${i} isn't a valid directory name` // regex detects invalid characters
 			global.World = require(`../plugins/${i}/main`)(global.World)
 		}
 	}
