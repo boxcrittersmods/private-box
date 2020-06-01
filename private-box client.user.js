9814300c89ad76a9e2b3af7fcbc8189f32cedd00
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-06-01 10:12:44 -0400

    cardboard

--- private-box client.user.js
+++ private-box client.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Private-box client
 // @namespace    http://tampermonkey.net/
-// @version      Alpha 1.0.0
+// @version      Alpha 1.1.0
 // @run-at       document-start
 // @description  connect to private-box servers
 // @author       SArpnt
 // @match        https://play.boxcritters.com/*
-// @require      https://code.jquery.com/jquery-3.5.1.min.js
 // @grant        none
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// @require      https://cdn.jsdelivr.net/gh/sarpnt/joinFunction/script.min.js
+// @require      https://cdn.jsdelivr.net/gh/sarpnt/EventHandler/script.min.js
+// @require      https://cdn.jsdelivr.net/gh/boxcritters/cardboard/script.user.min.js
 // ==/UserScript==
 
 (function () {
@@ -20,24 +23,8 @@
 	};
 	if (!url.ip) return; // nothing needs to happen if not connecting to a server
 
-	if (document.head) { // dumdum detector
-		alert('Enable instant script injection in Tampermonkey settings');
-		return;
-	}
-
-	let scripts = {
-		login: { src: '/scripts/login.js', text: null, done: false },
-		index: { src: 'index.js', text: null, done: false },
-	};
-	for (let i in scripts)
-		$.get(scripts[i].src, d => (scripts[i].text = d), 'text');
-
-	//if (!window.cardboard)// cardboard fixes world events
-	//	alert('Cardboard missing, mod compatibility is TERRIBLE with non-cardboard mods');
-
-	scripts.login.function = function (oldtag) {
-		var tag = document.createElement('script');
-		tag.innerHTML = scripts.login.text.replace(
+	cardboard.on('loadScriptLogin', function (tag) {
+		tag.innerHTML = tag.innerHTML.replace(
 			/(let|var|)\s+ticket\s*=\s*result\.data\.SessionTicket\s*[\n;]/,
 			`let data = result.data;
 			console.log({data});`
@@ -48,12 +35,9 @@
 				playerId: data.PlayFabId
 			},${JSON.stringify(url.data)})}`
 		);
-
-		oldtag.replaceWith(tag);
-	};
-	scripts.index.function = function (oldtag) {
-		var tag = document.createElement('script');
-		tag.innerHTML = scripts.index.text.replace(
+	});
+	cardboard.on('loadScriptIndex', function (tag) {
+		tag.innerHTML = tag.innerHTML.replace(
 			/socketURL\s*=\s*[^\n;\s]*\s*[;\n]/,
 			`socketURL = ${JSON.stringify(url.ip)};`
 		).replace(
@@ -61,18 +45,5 @@
 			`console.log({detail:event.detail});
 			world.login(event.detail);`
 		);
-
-		oldtag.replaceWith(tag);
 	};
-	new MutationObserver((m, o) => {
-		for (let i in scripts)
-			if (!scripts[i].done) {
-				let a = document.querySelector(`script[src='${scripts[i].src}']`);
-				if (a) {
-					scripts[i].function(a);
-					scripts[i].done = true;
-					if (Object.values(scripts).every(i => i.done)) o.disconnect();
-				}
-			}
-	}).observe(document.documentElement, { childList: true, subtree: true });
 })();
